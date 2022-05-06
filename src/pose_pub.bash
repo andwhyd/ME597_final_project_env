a6c36f979c01fcae60701871d20fea91f0bcf10a
Jalil <jalil.cg@gmail.com>
2022-05-06 18:14:33 -0400

    Updated script. Now it possible to pass 1 or 2 as argument to select between pose 1 and 2.

--- src/pose_pub.bash
+++ src/pose_pub.bash
@@ -6,6 +6,20 @@ POSE_1_Y=6.11
 POSE_2_X=-2.18
 POSE_2_Y=10.95
 
+if [ $1 -eq 1 ]
+  then
+    POSE_X=${POSE_1_X}
+    POSE_Y=${POSE_1_Y}
+    echo "Pusblishing POSE 1"
+fi
+if [ $1 -eq 2 ]
+  then
+    POSE_X=${POSE_2_X}
+    POSE_Y=${POSE_2_Y}
+    echo "Pusblishing POSE 2"
+fi
+
+
 rostopic pub -1 /move_base_simple/goal geometry_msgs/PoseStamped "header:
   seq: 0
   stamp:
@@ -14,8 +28,8 @@ rostopic pub -1 /move_base_simple/goal geometry_msgs/PoseStamped "header:
   frame_id: ''
 pose:
   position:
-    x: ${POSE_2_X}
-    y: ${POSE_2_Y}
+    x: ${POSE_X}
+    y: ${POSE_Y}
     z: 0.0
   orientation:
     x: 0.0
